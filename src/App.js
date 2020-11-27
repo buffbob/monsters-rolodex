9d4a0ddf3a97f335381d607b173802a7b055f582
buffbob <lastgulch@gmail.com>
2020-11-26 17:32:26 -0800

    initial commit. seems to work. This is project1 from Udemy React course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './Components/card-list/card-list.component.js';
+import { SearchBox } from './Components/search-box/search-box.component.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { monsters: [], searchField: '' };
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1 className="AppTitle">Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search for monsters'
+          handleChange={this.handleChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </SearchBox>
+        <CardList monsters={filteredMonsters} >
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
